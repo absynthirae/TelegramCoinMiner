dcb59da2c79c30e7e90df73f6ea20911fa5e9806
greedox <No.Name1992@yandex.ru>
2020-02-08 22:02:24 +0300

    Copy previous project

--- TelegramCoinMiner/Program.cs
+++ TelegramCoinMiner/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+using TelegramCoinMiner;
 
 namespace TelegramCoinMiner
 {
@@ -19,9 +24,76 @@ namespace TelegramCoinMiner
 
             wrapper.InitializeBrowser();
 
-            string[] urls = await wrapper.GetResultAfterPageLoad("https://yandex.ru", async () =>
-                await wrapper.EvaluateJavascript<string[]>("$('a[href]').map((index, element) => $(element).prop('href')).toArray()"));
+            int apiId = 1038521;
+            string apiHash = "e365dd8b6c6336da17a4537f5fae2870"; //API-key Tema
 
+            #region TargetData
+
+            string botName = "BTC Click Bot";
+
+            #endregion
+
+            Console.WriteLine("Enter phone number as +71234567890");
+            string phone = Console.ReadLine();
+
+            var client = new TelegramClient(apiId, apiHash, sessionUserId: phone); //cleint
+            await client.ConnectAsync();
+
+            if (!File.Exists(phone + ".dat"))
+            {
+                var hash = await client.SendCodeRequestAsync(phone);
+                Console.WriteLine("Enter telegram code");
+                string code = Console.ReadLine();
+                var user = await client.MakeAuthAsync(phone, hash, code);
+            }
+
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+
+            var channel = dialogs.Users
+                .OfType<TLUser>()
+                .FirstOrDefault(c => c.FirstName == botName);
+
+            TLMessagesSlice res = await client.GetMessages((long)channel.AccessHash, channel.Id);
+
+
+            foreach (var mes in res.Messages)
+            { //смотрим сообщение
+                TLMessage sms = mes as TLMessage;
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine(sms.Message); //вывод сообщений
+
+                TLReplyInlineMarkup Mark = sms.ReplyMarkup as TLReplyInlineMarkup;
+                if (Mark == null) { continue; } //Если сообщение не содержит кнопок со ссылками
+                var rows = Mark.Rows;
+
+                foreach (TLKeyboardButtonRow keyRow in rows)  //бегаем по строкам 
+                {
+                    TLVector<TLAbsKeyboardButton> buttons = keyRow.Buttons;  //берем все кнопки
+
+                    Console.WriteLine($"Buttons {buttons.Count}");
+                    if (buttons.Count <= 0) { continue; };
+
+                    foreach (TLAbsKeyboardButton keyBtn in buttons) //бегаем по взятым кнопкам 
+                    {
+                        if (!(keyBtn is TLKeyboardButtonUrl)) { continue; }
+                        var key = (TLKeyboardButtonUrl)keyBtn;
+                        if (key.Text.Contains("Go to website"))
+                        { //если кнопка содержит текст то:
+                            /*Переход по ссылке*/
+                            /*
+                            Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" + " " + key.Url);
+                            await Task.Delay(15000);
+                            foreach (var proc in Process.GetProcessesByName("Chrome.exe")) {
+                            proc.Kill();
+                            }
+                            */
+                            Console.WriteLine("Был переход по ссылке:" + key.Url);
+                            await wrapper.GetResultAfterPageLoad(key.Url, async () => { return Task.Delay(15000); });
+                        }
+                    }
+                }
+                Console.WriteLine(sms.Message);
+            }
             wrapper.ShutdownBrowser();
         }
     }
---
+++ TelegramCoinMiner/TelegramClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+
+namespace TelegramCoinMiner
+{
+    static class TelegramClientExtensions
+    {
+        public async static Task<TLMessagesSlice> GetMessages(this TelegramClient client, long access_hash, int user_id)
+        {
+            return await client.SendRequestAsync<TLMessagesSlice>(new TLRequestGetHistory()
+            {
+                Peer = new TeleSharp.TL.TLInputPeerUser { AccessHash = access_hash, UserId = user_id },
+                Limit = 5,
+                AddOffset = 0,
+                OffsetId = 0
+            });
+        }
+    }
+}

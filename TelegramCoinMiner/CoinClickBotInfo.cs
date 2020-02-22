8e48b9e2bcf48df2ff1eb8939898510ad268dfcb
greedox <No.Name1992@yandex.ru>
2020-02-22 10:32:10 +0300

    Add factory methods for CoinClickBotInfo

--- TelegramCoinMiner/CoinClickBotInfo.cs
+++ TelegramCoinMiner/CoinClickBotInfo.cs
@@ -10,12 +10,30 @@ namespace TelegramCoinMiner
     {
         public string BotName { get; set; }
         public int ReadMessagesCount { get; set; }
-     
+
         public static CoinClickBotInfo CreateBitcoinClickBotInfo()
         {
-            return new CoinClickBotInfo { BotName = "BTC Click Bot" , ReadMessagesCount=2};
+            return new CoinClickBotInfo { BotName = "BTC Click Bot" , ReadMessagesCount = 2 };
+        }
+        
+        public static CoinClickBotInfo CreateBitcoinCashClickBotInfo()
+        {
+            return new CoinClickBotInfo { BotName = "BCH Click Bot", ReadMessagesCount = 2 };
         }
 
-        
+        public static CoinClickBotInfo CreateDogecoinClickBotInfo()
+        {
+            return new CoinClickBotInfo { BotName = "DOGE Click Bot", ReadMessagesCount = 2 };
+        }
+
+        public static CoinClickBotInfo CreateZcashClickBotInfo()
+        {
+            return new CoinClickBotInfo { BotName = "ZEC Click Bot", ReadMessagesCount = 2 };
+        }
+
+        public static CoinClickBotInfo CreateLitecoinClickBotInfo()
+        {
+            return new CoinClickBotInfo { BotName = "LTC Click Bot", ReadMessagesCount = 2 };
+        }
     }
 }

ea54ea67ff52c972bd62ba5107dd49003424aef8
greedox <No.Name1992@yandex.ru>
2020-02-23 20:53:29 +0300

    Add Worker class

---
+++ TelegramCoinMiner/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner
+{
+    public class Worker
+    {
+        public bool IsWorks { get => _tokenSource.Token.IsCancellationRequested; }
+
+        private Task _workerThread;
+        private CancellationTokenSource _tokenSource;
+
+        public void Start(Action workerJob)
+        {
+            if (_tokenSource != null)
+            {
+                Stop();
+            }
+
+            _tokenSource = new CancellationTokenSource();
+            InitializeWorkerThread(workerJob);
+            _workerThread.Start();
+        }
+
+        private void InitializeWorkerThread(Action workerJob)
+        {
+            _workerThread = new Task(() =>
+            {
+                while (!_tokenSource.Token.IsCancellationRequested)
+                {
+                    workerJob();
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+            _workerThread.Wait();
+            _tokenSource.Dispose();
+            _workerThread.Dispose();
+            _tokenSource = null;
+            _workerThread = null;
+        }
+    }
+}

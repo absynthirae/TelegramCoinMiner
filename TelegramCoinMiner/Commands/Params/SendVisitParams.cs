8fc3dc56b6792e353d83285cdb8207bfabf4c055
greedox <No.Name1992@yandex.ru>
2020-02-23 21:42:09 +0300

    Add ICommand interface and implement SendVisitCommand

---
+++ TelegramCoinMiner/Commands/Params/SendVisitParams.cs
@@ -0,0 +1,11 @@
+﻿using TeleSharp.TL;
+using TLSharp.Core;
+
+namespace TelegramCoinMiner.Commands.Params
+{
+    public class SendVisitParams
+    {
+        public TelegramClient TelegramClient { get; set; }
+        public TLInputPeerChannel Channel { get; set; }
+    }
+}
---
+++ TelegramCoinMiner/Commands/SendVisitCommand.cs
@@ -0,0 +1,19 @@
+﻿using TelegramCoinMiner.Commands.Params;
+
+namespace TelegramCoinMiner.Commands
+{
+    public class SendVisitCommand : ICommand
+    {
+        public SendVisitParams Params { get; set; }
+
+        public SendVisitCommand(SendVisitParams sendVisitParams)
+        {
+            Params = sendVisitParams;
+        }
+
+        public void Execute()
+        {
+            Params.TelegramClient.SendMessageAsync(Params.Channel, "/visit").Wait();
+        }
+    }
+}

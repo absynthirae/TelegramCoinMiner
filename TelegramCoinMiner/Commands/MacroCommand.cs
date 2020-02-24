de9af178e4ac6a8b060a5a05e625246da30d1d4c
greedox <No.Name1992@yandex.ru>
2020-02-24 03:24:55 +0300

    Add MacroCommand

---
+++ TelegramCoinMiner/Commands/MacroCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner.Commands
+{
+    public class MacroCommand : ICommand
+    {
+        private List<ICommand> commands;
+
+        public MacroCommand(List<ICommand> commands)
+        {
+            this.commands = commands;
+        }
+
+        public void Execute()
+        {
+            foreach (ICommand c in commands)
+                c.Execute();
+        }
+    }
+}

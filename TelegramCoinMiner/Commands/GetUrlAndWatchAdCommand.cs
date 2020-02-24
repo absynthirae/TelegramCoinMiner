4fc2a7dc79fc5faf5920e617d95fcfc8753e06bd
greedox <No.Name1992@yandex.ru>
2020-02-24 03:06:18 +0300

    Add GetUrlAndWatchAdCommand without implementation

---
+++ TelegramCoinMiner/Commands/GetUrlAndWatchAdCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner.Commands
+{
+    public class GetUrlAndWatchAdCommand : ICommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

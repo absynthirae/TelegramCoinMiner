b67f2ef3d904b2c322613a397608e228310b1f36
Konsyl <615153@mail.ru>
2020-02-24 12:32:11 +0300

    StartInfo

---
+++ TelegramCoinMiner/Information/Channel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner.Information
+{
+   public class Channel
+    {
+        string visitWord { get; set; }
+        string gkipWord { get; set; }
+        string goWord { get; set; }
+
+        string botName
+        public Channel() { 
+        
+        }
+        public Channel(string VisitWord="/visit", string SkipWord = "/skip", string GoWord="go to website" )
+        {
+            this.visitWord = VisitWord;
+            this.gkipWord = SkipWord;
+            this.goWord = GoWord;
+        }
+  
+    }
+}
---
+++ TelegramCoinMiner/Information/InfoCreator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner.Information
+{
+    static class InfoCreator
+    {
+        static int _current = 0;
+
+        static List<Channel> _queue = new List<Channel>
+        {
+            new Channel(goWord: "go to website", skipWord: "/skip", visitWord: "/visit", messageCount:2),
+            new Channel(goWord: "go to website", skipWord: "/skip", visitWord: "/visit", messageCount:2),
+            new Channel(goWord: "go to website", skipWord: "/skip", visitWord: "/visit", messageCount:2),
+            new Channel(goWord: "go to website", skipWord: "/skip", visitWord: "/visit", messageCount:2),
+            new Channel(goWord: "go to website", skipWord: "/skip", visitWord: "/visit", messageCount:2)
+        };
+
+        static public Channel GetNextChannel()
+        {
+
+            _current += (_current + 1) % _queue.Count;
+            return _queue[_current];
+
+
+        }
+        static public void AddChannel(Channel cn)
+        {
+            _queue.Add(cn);
+
+        }
+
+        static public void DeleteChannel(Channel cn)
+        {
+            _queue.Remove(cn);
+        }
+
+    }
+}

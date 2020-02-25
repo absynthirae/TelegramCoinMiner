495fa61fcbf4bf7a45787121bf38fdb6377fbb08
Konsyl <615153@mail.ru>
2020-02-25 18:51:28 +0300

    add Constants and Exceptions

---
+++ TelegramCoinMiner/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner
+{
+    public static class Constants
+    {
+
+        const int browserLoadTimeOut = 60000;
+        const int readMessageCount = 2;
+        const int standartTimeDelay = 1500;
+
+        const string nameDogecoin = "@Dogecoin_click_bot";
+        const string nameLitecoin = "@Litecoin_click_bot";
+        const string nameBchcoin  = "@BCH_clickbot";
+        const string nameScushcoin = "@Zcash_click_bot";
+        const string nameBitcoin  = "@BitcoinClick_bot";
+
+        const string titleDogecoin = "DOGE Click Bot";
+        const string titleLitecoin = "LTC Click Bot";
+        const string titleBchcoin = "BCH Click Bot";
+        const string titleScushcoin = "ZEC Click Bot";
+        const string titleBitcoin = "BTC Click Bot";
+
+    }
+}
---
+++ TelegramCoinMiner/Exceptions/CapchaException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCoinMiner.Exceptions
+{
+    class CapchaException :Exception
+    {
+
+        public override string Message => "Capcha is here";
+        public override string ToString()
+        {
+
+            return Message;
+        }
+
+    }
+}

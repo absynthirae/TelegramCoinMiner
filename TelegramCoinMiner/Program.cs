a6a1511627c32af508b5f372a5f2bd1703c4688a
greedox <No.Name1992@yandex.ru>
2020-02-26 22:45:51 +0300

    Remove unused classes

--- TelegramCoinMiner/Program.cs
+++ TelegramCoinMiner/Program.cs
@@ -7,30 +7,22 @@ using TelegramCoinMiner.Commands.Params;
 namespace TelegramCoinMiner
 {
     class Program
-    {
-        static CefSharpWrapper wrapper = new CefSharpWrapper();
-        
+    {        
         private static void Main()
         {
-            wrapper.InitializeBrowser();
             MainAsync().Wait();
             Console.ReadKey();
-            wrapper.ShutdownBrowser();
+            WebBrowserFactory.ShutdownWebBrowsers();
         }
 
         private static async Task MainAsync()
         {
             int apiId = 1038521;
-
             string apiHash = "e365dd8b6c6336da17a4537f5fae2870"; //API-key Tema
 
-            Console.WriteLine("Enter phone number as +71234567890");
+            Console.WriteLine("Enter phone number as 79123456789");
 
             string phone = Console.ReadLine();
-
-            //TelegramClientWrapper telegramClient = new TelegramClientWrapper(apiId, apiHash, phone, wrapper._browser);
-
-            //await telegramClient.Start();
 
             var client = await TelegramClientFactory.CreateTelegramClientAsync(phone);
 
@@ -38,7 +30,7 @@ namespace TelegramCoinMiner
                 new LaunchClickBotParams
                 {
                     TelegramClient = client,
-                    Browser = wrapper._browser,
+                    Browser = WebBrowserFactory.CreateWebBrowser(),
                     TokenSource = new CancellationTokenSource()
                 }
             });
@@ -47,7 +39,6 @@ namespace TelegramCoinMiner
 
             if (Console.ReadKey().Key == ConsoleKey.Escape) 
             {
-                //telegramClient.Stop();
                 workerPool.Stop();
                 Console.WriteLine("Вы прервали процесс");
             }

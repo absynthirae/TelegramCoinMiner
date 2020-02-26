6918ce152fa1298392606fbf0b20fa7a1499294c
greedox <No.Name1992@yandex.ru>
2020-02-26 14:21:20 +0300

    Add TelegramLoginParams

---
+++ TelegramCoinMiner/TelegramLoginParams.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramCoinMiner
+{
+    public class TelegramLoginParams
+    {
+        public string PhoneNumber { get; set; }
+        public int ApiId { get; set; }
+        public string ApiHash { get; set; }
+    }
+}

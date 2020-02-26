251d369bf5d7f831f5b61398368abee0d62f3343
greedox <No.Name1992@yandex.ru>
2020-02-26 14:39:01 +0300

    TelegramClientFactory

---
+++ TelegramCoinMiner/TelegramClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TLSharp.Core;
+
+namespace TelegramCoinMiner
+{
+    static class TelegramClientFactory
+    {
+        public static List<TelegramClient> TelegramClients { get; set; }
+
+        public static async Task<TelegramClient> CreateTelegramClientAsync(string phoneNumber)
+        {
+            var client = TelegramClients.FirstOrDefault(x => x.Session.TLUser.Phone == phoneNumber);
+            if (client != null)
+            {
+                return client;
+            }
+
+            int apiId = 907056;
+            string apiHash = "697525d840e31523f43a972dff47e16a";
+
+            client = new TelegramClient(apiId, apiHash, sessionUserId: phoneNumber);
+            await client.ConnectAsync();
+            if (!client.IsUserAuthorized())
+            {
+                throw new System.Exception("Session not found");
+            }
+            TelegramClients.Add(client);
+            return client;
+        }
+    }
+}
